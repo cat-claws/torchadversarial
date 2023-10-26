3e6345068646f4c707f5ac7d08eb25c88c94bccd
cat-claws <47313357+cat-claws@users.noreply.github.com>
2023-10-26 22:24:11 +0800

    Create attack.py

---
+++ torchadversarial/attack.py
@@ -0,0 +1,25 @@
+from collections.abc import Iterator
+
+class Attack(Iterator):
+	def __init__(
+		self,
+		optimizer,
+		params,
+		**kwargs,
+	):
+		self._index = 0
+		self.params = [param.detach().clone().requires_grad_() for param in params]
+		self.optimizer = optimizer(self.params, **kwargs)
+		self.steps = kwargs.get('steps', 1)
+	
+	def __next__(self):
+		if self._index > 0:
+			self.optimizer.step()
+		self.optimizer.zero_grad()
+		if self._index < self.steps:
+			self._index += 1
+			return self.params
+		else:
+			for param in self.params:
+				param.requires_grad = False
+			raise StopIteration
